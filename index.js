33308b5cbd80d907d46fbd9d590cef1b529b5dd4
AnnelyKurg <annely.kurg@voco.ee>
2022-04-19 21:11:58 +0300

    WebSocet - first step

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app= express();
 
-/*app.get("/", (req, res) => {
-   res.send("Hello World");
-});*/
-
-app.get("/about", (req, res) => {
-   res.send("About route 2");
+app.get("/", (req, res) => {
+   res.send("Hello World 1 2");
 });
+
+
+
+/*app.get("/about", (req, res) => {
+   res.send("About route 2");
+});*/
 
 app.listen(3000, () => console.log("Server is listening to port 3000"));
