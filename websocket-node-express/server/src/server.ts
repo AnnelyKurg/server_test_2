6827dfc18d561e4aadd122edb77866dd7258aca0
AnnelyKurg <annely.kurg@voco.ee>
2022-04-19 21:31:27 +0300

    Smart WebSocet Client and fist tests

--- websocket-node-express/server/src/server.ts
+++ websocket-node-express/server/src/server.ts
@@ -1,4 +1,4 @@
-import * as express from 'express';
+/*import * as express from 'express';
 import * as http from 'http';
 import * as WebSocket from 'ws';
 
@@ -27,4 +27,28 @@ wss.on('connection', (ws: WebSocket) => {
 //start our server
 server.listen(process.env.PORT || 8999, () => {
     console.log(`Server started on port ${server.address().port} :)`);
+});*/
+
+//connection is up, let's add a simple simple event
+ws.on('message', (message: string) => {
+
+    //log the received message and send it back to the client
+    console.log('received: %s', message);
+
+    const broadcastRegex = /^broadcast\:/;
+
+    if (broadcastRegex.test(message)) {
+        message = message.replace(broadcastRegex, '');
+
+        //send back the message to the other clients
+        wss.clients
+            .forEach(client => {
+                if (client != ws) {
+                    client.send(`Hello, broadcast message -> ${message}`);
+                }
+            });
+
+    } else {
+        ws.send(`Hello, you sent -> ${message}`);
+    }
 });
